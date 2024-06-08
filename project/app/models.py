0f14b34bf2349328276fccbc3e0e0cda6005c341
sauravpastor <157570587+sauravpastor@users.noreply.github.com>
2024-06-09 01:03:30 +0530

    final commit

--- project/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Employee(models.Model):
-    EmployeeName = models.CharField(max_length=50)
-    EmployeeSalary = models.IntegerField()
-    EmployeeRole = models.CharField(max_length=20)
-    EmployeePhone = models.IntegerField()
